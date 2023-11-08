aff45a688c8bf8a35d5029968fb832b8c25a2800
Leonardo Narciso <LNarcisoOV@gmail.com>
2023-11-07 22:49:16 -0300

    First commit.

---
+++ src/main/java/com/interceptor/SpringbootInterceptorStudyApplication.java
@@ -0,0 +1,13 @@
+package com.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterceptorStudyApplication.class, args);
+	}
+
+}
